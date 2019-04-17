43eaae9850b5306b2749bbb92169c441df918efd
Zach Ferraro <zmferraro@gmail.com>
2019-04-17 12:23:40 -0500

    -Remove module require

--- helloworld/app.js
+++ helloworld/app.js
@@ -3,13 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-if (module === require.main) {
-    const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-    });
-}
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
