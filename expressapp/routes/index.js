cbcb88a607238102e666dca684e5b9835a13d7d5
Zach Ferraro <zmferraro@gmail.com>
2019-04-22 15:14:55 -0500

    W3Schools.com To-Do JS  Example

--- expressapp/routes/index.js
+++ expressapp/routes/index.js
@@ -6,11 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-router.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
+// router.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+// router.post('/', function (req, res) {
+//   res.send('Got a POST request')
+// })
 
 module.exports = router;
